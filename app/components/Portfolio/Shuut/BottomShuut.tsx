ebcc17dafc40902ef726c0ca6edaef35d8f4e1ba
dreezy305 <bankoleidris@gmail.com>
2023-08-19 10:58:41 +0100

    shuut page contd

--- app/components/Portfolio/Shuut/BottomShuut.tsx
+++ app/components/Portfolio/Shuut/BottomShuut.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 import Image from "next/image";
+import "./shuut.css";
 import ShuutLanding from "../../../../public/shuut/ShuutLandingPage.png";
+import MaskGroup2 from "../../../../public/MaskGroup2.png";
+import MaskGroup3 from "../../../../public/MaskGroup3.png";
+import MaskGroup4 from "../../../../public/MaskGroup4.png";
+import MaskGroup5 from "../../../../public/MaskGroup5.png";
 
 function BottomShuut(): JSX.Element {
   return (
@@ -73,6 +78,86 @@ function BottomShuut(): JSX.Element {
         All users on the system are meticulously verified before transactions
         are allowed to take place.
       </p>
+
+      <h5 className="font-LotaThin font-thin text-3xl leading-7 pt-12 text-center">
+        User Interview
+      </h5>
+
+      <div className="rounded-lg py-6 px-12 bg-BeautyDark flex flex-col space-y-5">
+        <p className="font-LotaRegular text-3xl font-normal py-2 w-full">
+          Do you have Equipment in your possession that you’re currently not
+          using?
+        </p>
+
+        <p className="font-LotaRegular text-3xl font-normal py-2">
+          Why would you choose to rent out your equipment?
+        </p>
+
+        <p className="font-LotaRegular text-4xl font-normal py-2">
+          What was your experience renting your equipments?
+        </p>
+
+        <p className="font-LotaRegular text-4xl font-normal py-2">
+          Have you ever manhandled someone’s equipment before?
+        </p>
+
+        <div className="flex flex-row items-center justify-between space-x-4 pt-6">
+          <div className="rounded-lg py-4 h-auto w-1/2 px-4 bg-BeautyDarkPurple relative">
+            <p className="font-LotaRegular font-normal text-lg leading-7">
+              Yes, i have
+            </p>
+            <Image
+              src={MaskGroup2}
+              height={30}
+              width={30}
+              alt="mask"
+              className="absolute top-left"
+            />
+          </div>
+
+          <div className="rounded-lg py-6 px-4 w-1/2 bg-BeautyDarkPurple relative h-auto">
+            <p className="font-LotaRegular font-normal text-lg leading-7 w-full">
+              I choose to rent my equipment as another source of income to me
+            </p>
+            <Image
+              src={MaskGroup3}
+              height={30}
+              width={30}
+              alt="mask"
+              className="absolute bottom-right"
+            />
+          </div>
+        </div>
+        <div className="flex flex-row items-center justify-between space-x-4 pb-6">
+          <div className="rounded-lg py-6 px-4 w-1/2 bg-BeautyDarkPurple relative h-auto">
+            <p className="font-LotaRegular font-normal text-lg leading-7 w-full">
+              There have been good days and bad days when renting, as most
+              customers don't have the knowledge of handling the equipments,
+              thereby leading to malfunction of some parts
+            </p>
+            <Image
+              src={MaskGroup4}
+              height={30}
+              width={30}
+              alt="mask"
+              className="absolute top-right"
+            />
+          </div>
+
+          <div className="rounded-lg py-6 px-4 w-1/2 bg-BeautyDarkPurple relative h-auto">
+            <p className="font-LotaRegular font-normal text-lg leading-7 w-full">
+              No, i have not
+            </p>
+            <Image
+              src={MaskGroup5}
+              height={30}
+              width={30}
+              alt="mask"
+              className="absolute bottom-right"
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
