54e299f38149fd52d59ea37db845ce49f033940e
dreezy305 <bankoleidris@gmail.com>
2023-09-16 16:18:31 +0100

    woodcore responsiveness

--- app/components/Portfolio/woodcore/TopWoodcore.tsx
+++ app/components/Portfolio/woodcore/TopWoodcore.tsx
@@ -4,7 +4,8 @@ import "./woodcore.css";
 
 function TopWoodcore(): JSX.Element {
   return (
-    <div className="rounded-tl-[30px] rounded-tr-[30px] rounded-br-none rounded-bl-none py-6 px-7 h-72 w-full bg-white mt-10 woodcore-image-background">
+    <>
+    <div className="hidden md:hidden lg:block xl:block rounded-tl-[30px] rounded-tr-[30px] rounded-br-none rounded-bl-none py-6 px-7 h-72 w-full bg-white mt-10 woodcore-image-background">
       <div className="w-1/2 flex flex-col space-y-5 pt-5">
         <Image
           src={"/woodcorelogo.svg"}
@@ -22,6 +23,26 @@ function TopWoodcore(): JSX.Element {
         </div>
       </div>
     </div>
+
+    <div className="block md:block lg:hidden xl:hidden rounded-tl-[30px] rounded-tr-[30px] rounded-br-none rounded-bl-none py-6 px-7  w-full bg-white mt-10">
+      <div className="w-full flex flex-col space-y-5 pt-5">
+        <Image
+          src={"/woodcorelogo.svg"}
+          alt="layer"
+          width={100}
+          height={24}
+          priority
+        />
+        <div>
+          <p className="font-LotaRegular font-normal text-sm md:text-base leading-8 text-BeautyDeepBlue w-[102%] md:w-11/12 ">
+            WoodCore is a banking application for
+           internal staffs (Agents & Branch Managers) that helps
+            them manage their users.
+          </p>
+        </div>
+      </div>
+    </div>
+    </>
   );
 }
 
