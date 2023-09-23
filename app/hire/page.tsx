588acda4cc658d7d39ba0c7ca9cd4e0824e1ca47
dreezy305 <bankoleidris@gmail.com>
2023-09-23 10:24:07 +0100

    animate

--- app/hire/page.tsx
+++ app/hire/page.tsx
@@ -17,7 +17,7 @@ function HireMe(): JSX.Element {
             <div className="flex flex-col space-y-8 md:flex md:flex-col md:space-y-8 lg:flex lg:flex-row lg:items-center lg:justify-between xl:flex xl:flex-row xl:items-center xl:justify-between pt-3 mt-2 md:mt-4 lg:mt-6 xl:mt-6">
               <div className="flex flex-col space-y-5 md:space-y-6 lg:space-y-8 xl:space-y-8 w-full md:w-full lg:w-1/2 xl:w-1/2">
                 <div className="flex flex-row items-center space-x-2 md;space-x-2 lg:space-x-0 xl:space-x-0">
-                  <h4 className="font-LotaBold text-BeautyWhite text-3xl md:text-4xl lg:text-7xl xl:text-7xl leading-9">
+                  <h4 className="font-LotaBold text-BeautyWhite text-3xl md:text-4xl lg:text-7xl xl:text-7xl leading-9 animate__animated animate__fadeInDown">
                     Say Hello.
                   </h4>
                   <Image
@@ -37,11 +37,11 @@ function HireMe(): JSX.Element {
                     className="block md:block lg:hidden xl:hidden"
                   />
                 </div>
-                <p className="font-SofiaRegular font-normal w-full text-xl leading-9">
+                <p className="font-SofiaRegular font-normal w-full text-xl leading-9 animate__animated animate__lightSpeenInLeft">
                   Let's have a chat over coffee.
                 </p>
 
-                <div className="flex flex-col space-y-3 bg-BeautyDarkPurple rounded-lg px-6 py-4 w-full md:w-2/3 lg:w-1/2 xl:w-1/2">
+                <div className="flex flex-col space-y-3 bg-BeautyDarkPurple rounded-lg px-6 py-4 w-full md:w-2/3 lg:w-1/2 xl:w-1/2 animate__animated animate__slideInDown">
                   <h5 className="font-SofiaRegular font-normal text-xl">
                     WANT TO EMAIL ME?
                   </h5>
@@ -50,7 +50,7 @@ function HireMe(): JSX.Element {
                   </p>
                 </div>
 
-                <div className="flex flex-col space-y-3 bg-BeautyDarkPurple rounded-lg px-6 py-4 w-full md:w-2/3 lg:w-1/2 xl:w-1/2">
+                <div className="flex flex-col space-y-3 bg-BeautyDarkPurple rounded-lg px-6 py-4 w-full md:w-2/3 lg:w-1/2 xl:w-1/2 animate__animated animate__slideInUp">
                   <h5 className="font-SofiaRegular font-normal text-xl">
                     WANT TO CALL ME?
                   </h5>
@@ -64,7 +64,7 @@ function HireMe(): JSX.Element {
                     href="https://www.behance.net/ohiabeauty"
                     target="_blank"
                     rel="noreferrer"
-                    className="cursor-pointer"
+                    className="cursor-pointer animate__animated animate__fadeIn"
                   >
                     <BrandBehance
                       size={40}
@@ -77,7 +77,7 @@ function HireMe(): JSX.Element {
                     href="https://medium.com/@ohiabeauty26"
                     target="_blank"
                     rel="noreferrer"
-                    className="cursor-pointer"
+                    className="cursor-pointer cursor-pointer animate__animated animate__fadeIn"
                   >
                     <BrandMedium
                       size={40}
@@ -89,7 +89,7 @@ function HireMe(): JSX.Element {
                   <a
                     target="_blank"
                     rel="noreferrer"
-                    className="cursor-pointer"
+                    className="cursor-pointer cursor-pointer animate__animated animate__fadeIn"
                     href="https://www.linkedin.com/in/beautyohia/"
                   >
                     <BrandLinkedin
@@ -103,7 +103,7 @@ function HireMe(): JSX.Element {
                   <a
                     target="_blank"
                     rel="noreferrer"
-                    className="cursor-pointer"
+                    className="cursor-pointer cursor-pointer animate__animated animate__fadeIn"
                     href="https://twitter.com/OhiaBeauty"
                   >
                     <BrandTwitter
@@ -117,7 +117,7 @@ function HireMe(): JSX.Element {
                   <a
                     target="_blank"
                     rel="noreferrer"
-                    className="cursor-pointer"
+                    className="cursor-pointer cursor-pointer animate__animated animate__fadeIn"
                     href="https://twitter.com/OhiaBeauty"
                   >
                     <BrandInstagram
@@ -129,14 +129,14 @@ function HireMe(): JSX.Element {
                   </a>
                 </div>
               </div>
-              <div className="pt-6 md:pt-5 lg:pt-0 xl:pt-0 pb-6 md:pb-5 lg:pb-0 xl:pb-0  mt-14 md:mt-12 lg:mt-0 xl:mt-0 w-full md:w-full lg:w-1/2 xl:w-1/2 flex flex-row justify-center md:justify-center lg:justify-end xl:justify-end h-auto">
+              <div className="pt-6 md:pt-5 lg:pt-0 xl:pt-0 pb-6 md:pb-5 lg:pb-0 xl:pb-0  mt-14 md:mt-12 lg:mt-0 xl:mt-0 w-full md:w-full lg:w-1/2 xl:w-1/2 flex flex-row justify-center md:justify-center lg:justify-end xl:justify-end h-auto animate__animated animate__bounceInDown">
                 <Image
                   src={"/hello.png"}
                   priority
                   height={500}
                   width={500}
                   alt="hello"
-                  className="pt-9 md:pt-6 lg:pt-0 xl:pt-0 pb-9 md:pb-6 lg:pb-0 xl:pb-0"
+                  className="pt-9 md:pt-6 lg:pt-0 xl:pt-0 pb-9 md:pb-6 lg:pb-0 xl:pb-0 animate__animated animate__bounceInDown"
                 />
               </div>
             </div>
