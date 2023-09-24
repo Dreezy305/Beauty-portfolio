bd9434991fe21cf3b74c4211dd471b6ac8d16bcb
dreezy305 <bankoleidris@gmail.com>
2023-09-24 22:52:46 +0100

    fixes: portfolio

--- app/components/Portfolio/Fara/TopFara.tsx
+++ app/components/Portfolio/Fara/TopFara.tsx
@@ -4,7 +4,8 @@ import "./fara.css";
 
 function TopFara(): JSX.Element {
   return (
-    <div className="rounded-tl-[30px] rounded-tr-[30px] rounded-br-none rounded-bl-none py-6 px-5 md:px-6 lg:px-7 xl:px-7 h-56 w-full bg-BeautyDeepBlue mt-10 lg:fara-image-background xl:fara-image-background">
+    <>
+    <div className="rounded-tl-[30px] rounded-tr-[30px] rounded-br-none rounded-bl-none py-6 px-5 md:px-6 lg:px-7 xl:px-7 h-56 w-full bg-BeautyDeepBlue mt-10 lg:hidden xl:hidden">
       <div className="w-full md:w-4/5 lg:w-1/2 xl:w-1/2 flex flex-col space-y-5 pt-5">
         <Image
           src={"/faralogo.svg"}
@@ -21,6 +22,25 @@ function TopFara(): JSX.Element {
         </div>
       </div>
     </div>
+
+    <div className="hidden md:hidden lg:block xl:block rounded-tl-[30px] rounded-tr-[30px] rounded-br-none rounded-bl-none py-6 px-5 md:px-6 lg:px-7 xl:px-7 h-56 w-full bg-BeautyDeepBlue mt-10 fara-image-background">
+      <div className="lg:w-1/2 xl:w-1/2 flex flex-col space-y-5 pt-5">
+        <Image
+          src={"/faralogo.svg"}
+          alt="layer"
+          width={100}
+          height={24}
+          priority
+        />
+        <div>
+          <p className="w-2/3 font-LotaRegular font-normal  lg:text-lg xl:text-lg leading-6 md:leading-7 lg:leading-8 xl:leading-8 text-BeautyWhite pt-4 lg:pt-0 xl:pt-0 pb-4 md:pb-4 lg:pb-8 xl:pb-8">
+            The faster & easier way people are able to accesss
+            essentials digital services, to better theri quality of life.
+          </p>
+        </div>
+      </div>
+    </div>
+    </>
   );
 }
 
--- app/components/Portfolio/Shuut/TopShuut.tsx
+++ app/components/Portfolio/Shuut/TopShuut.tsx
@@ -5,7 +5,8 @@ import Group from "../../../../public/iPhone14Pro.png";
 
 function TopShuut(): JSX.Element {
   return (
-    <div className="rounded-tl-[30px] rounded-tr-[30px] rounded-br-none rounded-bl-none py-6 px-7 h-72 w-full bg-BeautyDeepPurple mt-10 flex flex-row lg:shuut-image-background relative">
+    <>
+    <div className="rounded-tl-[30px] rounded-tr-[30px] rounded-br-none rounded-bl-none py-6 px-7 h-72 w-full bg-BeautyDeepPurple mt-10 flex flex-row lg:shuut-image-background relative lg:hidden xl:hidden">
       <div className="w-full md:w-4/5 lg:w-1/2 xl:w-1/2 flex flex-col space-y-5 pt-5">
         <Image
           src={"/shuut.svg"}
@@ -23,6 +24,26 @@ function TopShuut(): JSX.Element {
         </div>
       </div>
     </div>
+
+    <div className="hidden md:hidden lg:block xl:block rounded-tl-[30px] rounded-tr-[30px] rounded-br-none rounded-bl-none py-6 px-7 h-72 w-full bg-BeautyDeepPurple mt-10 flex flex-row shuut-image-background relative">
+      <div className="w-full md:w-4/5 lg:w-1/2 xl:w-1/2 flex flex-col space-y-5 pt-5">
+        <Image
+          src={"/shuut.svg"}
+          alt="layer"
+          width={100}
+          height={24}
+          priority
+        />
+        <div>
+          <p className="font-LotaRegular font-normal text-sm md:text-base lg:text-lg xl:text-lg text-BeautyWhite leading-6 md:leading-7 lg:leading-8 xl:leading-8 pt-4 md:pt-0 lg:pt-0 xl:pt-0 pb-4 md:pb-4 lg:pb-8 xl:pb-8">
+            SHUUT is a peer-to-peer rental marketplace
+            which allows people to rent equipments to
+            others nearby, fully insured.
+          </p>
+        </div>
+      </div>
+    </div>
+    </>
   );
 }
 
