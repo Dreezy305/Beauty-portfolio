d15ba3a398b2c1da3426152a1dcc912c173e46a7
dreezy305 <bankoleidris@gmail.com>
2023-09-27 09:04:39 +0100

    fixes

--- app/components/unista/TopUnista.tsx
+++ app/components/unista/TopUnista.tsx
@@ -5,8 +5,8 @@ import "./unista.css";
 function TopUnista(): JSX.Element {
   return (
     <>
-      <div className="hidden md:block lg:block xl:block rounded-tl-[30px] rounded-tr-[30px] rounded-br-none rounded-bl-none py-6 px-7 h-60 w-full bg-BeautyUnista mt-10 unista-image-background">
-        <div className="w-1/2 flex flex-col space-y-5 pt-5">
+      <div className="hidden md:block lg:block xl:block rounded-tl-[30px] rounded-tr-[30px] rounded-br-none rounded-bl-none py-6 px-7 h-64 w-full bg-BeautyUnista mt-10 unista-image-background">
+        <div className="w-1/2 flex flex-col space-y-3 pt-5">
           <Image
             src={"/unistalogo.svg"}
             alt="layer"
@@ -15,7 +15,7 @@ function TopUnista(): JSX.Element {
             priority
           />
           <div>
-            <p className="font-LotaRegular font-normal md:w-[84%] md:text-xs lg:text-lg xl:text-lg md:leading-[16px] lg:leading-8 xl:leading-8 text-BeautyWhite">
+            <p className="font-LotaRegular font-normal md:w-[84%] md:text-xs lg:text-lg xl:text-lg md:leading-[16px] lg:leading-6 xl:leading-8 text-BeautyWhite">
             Unista is a banking application for internal staffs (Agents & Branch Managers) that helps them manage their users. over 4,000 agents are actively using the platform.
             </p>
           </div>
@@ -33,8 +33,7 @@ function TopUnista(): JSX.Element {
           />
           <div>
             <p className="font-LotaRegular font-normal text-sm leading-6 text-BeautyWhite w-11/12">
-              Unista is a banking application for internal staffs (Agents &
-              Branch Managers) that helps them manage their users.
+            Unista is a banking application for internal staffs (Agents & Branch Managers) that helps them manage their users. over 4,000 agents are actively using the platform.
             </p>
           </div>
         </div>
