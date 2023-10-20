77f6afb822fbdade3aab28fb0a2d7fe5431e9ce0
dreezy305 <bankoleidris@gmail.com>
2023-10-20 23:34:15 +0100

    tabs

--- app/components/development/Tab.tsx
+++ app/components/development/Tab.tsx
@@ -3,7 +3,6 @@ import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 
-
 function Tab({
   activeTab = "unista",
   onTabClick,
@@ -24,32 +23,83 @@ function Tab({
   }, []);
   return (
     <>
-      <ul className="w-full px-6 flex flex-row items-center justify-between h-20 bg-BeautyDarkPurple rounded-tl-2xl rounded-tr-2xl rounded-bl-none rounded-br-none">
-        <li
-          className={`cursor-pointer font-LotaRegular hover:text-BeautyDeepGrey text-xl relative portfolio-item ${activeTab === "unista"?"after:block after:content-[''] after:w-full after:h-1 after:absolute after:bottom-[-100%] after:left-0 after:bg-BeautyDeepGrey after:border text-BeautyDeepGrey after:border-BeautyDeepGrey after:ease-in-out after:duration-300":""} `}
-          onClick={() => router.push("/portfolio/development/unista")}
-        >
+      <div className="hidden md:block lg:block xl:block">
+        <ul className="w-full px-6 flex flex-row items-center justify-between h-20 bg-BeautyDarkPurple rounded-tl-2xl rounded-tr-2xl rounded-bl-none rounded-br-none">
+          <li
+            className={`cursor-pointer font-LotaRegular hover:text-BeautyDeepGrey text-xl relative portfolio-item ${
+              activeTab === "unista"
+                ? "after:block after:content-[''] after:w-full after:h-1 after:absolute after:bottom-[-100%] after:left-0 after:bg-BeautyDeepGrey after:border text-BeautyDeepGrey after:border-BeautyDeepGrey after:ease-in-out after:duration-300"
+                : ""
+            } `}
+            onClick={() => router.push("/portfolio/development/unista")}
+          >
+            Unista
+          </li>
+          <li
+            className={`cursor-pointer font-LotaRegular hover:text-BeautyDeepGrey text-xl relative portfolio-item ${
+              activeTab === "uplands"
+                ? "after:block after:content-[''] after:w-full after:h-1 after:absolute after:bottom-[-100%] after:left-0 after:bg-BeautyDeepGrey after:border text-BeautyDeepGrey after:border-BeautyDeepGrey after:ease-in-out after:duration-300"
+                : ""
+            } `}
+            onClick={() => router.push("/portfolio/development/uplands")}
+          >
+            Uplands Hunt
+          </li>
+          <li
+            className={`cursor-pointer font-LotaRegular hover:text-BeautyDeepGrey text-xl relative portfolio-item ${
+              activeTab === "woodcore"
+                ? "after:block after:content-[''] after:w-full after:h-1 after:absolute after:bottom-[-100%] after:left-0 after:bg-BeautyDeepGrey after:border text-BeautyDeepGrey after:border-BeautyDeepGrey after:ease-in-out after:duration-300"
+                : ""
+            } `}
+            onClick={() => router.push("/portfolio/development/woodcore")}
+          >
+            Woodcore
+          </li>
+          <li
+            className={`cursor-pointer font-LotaRegular hover:text-BeautyDeepGrey text-xl relative portfolio-item ${
+              activeTab === "quba"
+                ? "after:block after:content-[''] after:w-full after:h-1 after:absolute after:bottom-[-100%] after:left-0 after:bg-BeautyDeepGrey after:border text-BeautyDeepGrey after:border-BeautyDeepGrey after:ease-in-out after:duration-300"
+                : ""
+            }`}
+            onClick={() => router.push("/portfolio/development/quba")}
+          >
+            Quba
+          </li>
+        </ul>
+      </div>
+
+      <div className="block md:hidden lg:hidden xl:hidden">
+        <ul className="list-unstyled bg-BeautyDarkPurple rounded-2xl h-auto py-5 px-5">
+          <li className={`cursor-pointer font-LotaRegular hover:text-BeautyDeepGrey text-base relative py-2 ${
+              activeTab === "unista"
+                ? "before:block before:content-[''] before:h-5 before:absolute before:left-[-6.4%] before:bg-BeautyDeepGrey before:border text-BeautyDeepGrey before:border-BeautyDeepGrey before:ease-in-out before:duration-300 pl-1"
+                : "pl-1"
+            } `}  onClick={() => router.push("/portfolio/development/unista")}>
           Unista
-        </li>
-        <li
-         className={`cursor-pointer font-LotaRegular hover:text-BeautyDeepGrey text-xl relative portfolio-item ${activeTab === "uplands"?"after:block after:content-[''] after:w-full after:h-1 after:absolute after:bottom-[-100%] after:left-0 after:bg-BeautyDeepGrey after:border text-BeautyDeepGrey after:border-BeautyDeepGrey after:ease-in-out after:duration-300":""} `}
-          onClick={() => router.push("/portfolio/development/uplands")}
-        >
-          Uplands Hunt
-        </li>
-        <li
-          className={`cursor-pointer font-LotaRegular hover:text-BeautyDeepGrey text-xl relative portfolio-item ${activeTab === "woodcore"?"after:block after:content-[''] after:w-full after:h-1 after:absolute after:bottom-[-100%] after:left-0 after:bg-BeautyDeepGrey after:border text-BeautyDeepGrey after:border-BeautyDeepGrey after:ease-in-out after:duration-300":""} `}
-          onClick={() => router.push("/portfolio/development/woodcore")}
-        >
-          Woodcore
-        </li>
-        <li
-         className={`cursor-pointer font-LotaRegular hover:text-BeautyDeepGrey text-xl relative portfolio-item ${activeTab === "quba"?"after:block after:content-[''] after:w-full after:h-1 after:absolute after:bottom-[-100%] after:left-0 after:bg-BeautyDeepGrey after:border text-BeautyDeepGrey after:border-BeautyDeepGrey after:ease-in-out after:duration-300":""}`}
-          onClick={() => router.push("/portfolio/development/quba")}
-        >
-          Quba
-        </li>
-      </ul>
+          </li>
+          <li className={`cursor-pointer font-LotaRegular hover:text-BeautyDeepGrey text-base relative py-2 ${
+              activeTab === "uplands"
+                ? "before:block before:content-[''] before:h-5 before:absolute before:left-[-6.4%] before:bg-BeautyDeepGrey before:border text-BeautyDeepGrey before:border-BeautyDeepGrey before:ease-in-out before:duration-300 pl-1"
+                : "pl-1"
+            } `} onClick={() => router.push("/portfolio/development/uplands")}>
+            Uplands Hunt
+          </li>
+          <li className={`cursor-pointer font-LotaRegular hover:text-BeautyDeepGrey text-base relative py-2 ${
+              activeTab === "woodcore"
+                ? "before:block before:content-[''] before:h-5 before:absolute before:left-[-6.4%] before:bg-BeautyDeepGrey before:border text-BeautyDeepGrey before:border-BeautyDeepGrey before:ease-in-out before:duration-300 pl-1"
+                : "pl-1"
+            } `} onClick={() => router.push("/portfolio/development/woodcore")}>
+            Woodcore
+          </li>
+          <li className={`cursor-pointer font-LotaRegular hover:text-BeautyDeepGrey text-base relative py-2 ${
+              activeTab === "quba"
+                ? "before:block before:content-[''] before:h-5 before:absolute before:left-[-6.4%] before:bg-BeautyDeepGrey before:border text-BeautyDeepGrey before:border-BeautyDeepGrey before:ease-in-out before:duration-300 pl-1"
+                : "pl-1"
+            } `}  onClick={() => router.push("/portfolio/development/quba")}>
+            Quba
+          </li>
+        </ul>
+      </div>
 
       <div className="mb-6">{children}</div>
     </>
